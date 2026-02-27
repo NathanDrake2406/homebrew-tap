53a31e502bced0f8906f1e3640ed2fc14b6995b7
Nathan Nguyen <146415969+NathanDrake2406@users.noreply.github.com>
2026-02-27 21:38:07 +1100

    grove 0.3.1

--- Formula/grove.rb
+++ Formula/grove.rb
@@ -1,26 +1,26 @@
 class Grove < Formula
   desc "Cross-worktree conflict intelligence for git"
   homepage "https://github.com/NathanDrake2406/grove"
-  version "0.3.0"
+  version "0.3.1"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.0/grove-v0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "3cf0a1c46f3f8253d2d72fcae62dadbf8cd78512623ad4f24565890764bc7616"
+      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.1/grove-v0.3.1-aarch64-apple-darwin.tar.gz"
+      sha256 "9d097dc682ae0c50df463bc3a27fe99315f21fc9c43c2f6b2b7709d010747fb7"
     else
-      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.0/grove-v0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "dfd95777b652e1a4422505fd77e6cb6ce31f45dfbeffbf3c0d2767b7dcd6d6a6"
+      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.1/grove-v0.3.1-x86_64-apple-darwin.tar.gz"
+      sha256 "04247eaace00d373df5d11e9f559304d0c81c9ad8a592720cb86536de1478849"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.0/grove-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6f06cb5d7ff1a942257c8032a85583542ecf0ff09e1a42a246201984b6c7e428"
+      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.1/grove-v0.3.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "4a9e398f6989e164c025957c8be69bc9e0012ab62d0b6c87dcec2c12b9e66643"
     else
-      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.0/grove-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "735a71349ee0d91d928e4a89e867892868ff28745e09d4792e9a11fcca01a015"
+      url "https://github.com/NathanDrake2406/grove/releases/download/v0.3.1/grove-v0.3.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "ac527f5be4a458403fe28248d4addc3d285696a18032d57a4bd121b0dfb0faa5"
     end
   end
 
